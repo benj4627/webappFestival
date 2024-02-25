843b75a594e9b01ae294658fe0639e82e75cd845
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-02-25 15:48:23 +0100

    Tilføjede JS til nav og date picker

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+// //Finder de relevante button og list og gemmer dem i variabler
+// let allButtons = document.querySelectorAll(".option"); 
+// let allLists = document.querySelectorAll(".programList"); 
+
+// // Lytter efter click på alle knapperne via forEach, og når en knap bliver trykket på, looper den igennem programlists og gemmer i et array
+// allButtons.forEach(function(button) {
+//     button.addEventListener("click", function() {
+  
+//       allLists.forEach(function(list) {
+//         let itemsArray = Array.from(list.children);
+  
+//         // Bruger math.random til at sortere listerne tilfæligt
+//         itemsArray.sort(function() {
+//           return Math.random() - 0.5;
+//         });
+  
+//         // Tømmer indholdet i listerne og sætte nye placeringer ind
+//         list.innerHTML = "";
+//         list.append(...itemsArray);
+//       });
+//     });
+//   });
+ 
+
+
+//Vælger de relevante knapper i HTML
+let btnOptions = document.querySelectorAll('.option');
+
+//Looper igennem hver knap med forEach
+btnOptions.forEach(function(btnOption) {
+
+  //Tilføjer en eventlistener som lytter efter klik
+  btnOption.addEventListener("click", function() {
+
+    //Sikre at kun den klikkede knap har er aktiv og er farvet anderledes end de andre. Nulstiller alle knapper 
+    btnOptions.forEach(function(otherBtn) {
+        otherBtn.style.backgroundColor = "";
+        otherBtn.style.color = "";
+    });
+
+    // Farverne som skal tilføjes til den valgte knap
+    this.style.backgroundColor = "#FB8B24";
+    this.style.color = "white";
+  });
+});
+
+//Tjekker om den nuværende URL har "program" som en del af den
+if (window.location.href.includes('program')) {
+    //vælger ikoner i navigation og styler den som aktiv
+    let program = document.querySelector("#programIkon i");
+    program.style.color = "#FB8B24";
+
+    //vælger p elementet i navigationen og styler den som aktiv
+    let programPara = document.querySelector ("#programIkon p")
+    programPara.style.color = "#FB8B24";
+  }

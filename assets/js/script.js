6f22f4fda88fb6959332e29b6135467e38848e8d
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-02-25 16:48:36 +0100

    Færdiggjorde JS og venue info

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,12 @@ let btnOptions = document.querySelectorAll('.option');
 //Looper igennem hver knap med forEach
 btnOptions.forEach(function(btnOption) {
 
+    //tjekker om nogle af knapperne har "default" og hvis ja sætter default styling når siden loades. 
+   if (btnOption.classList.contains("default")) {
+    // Styler default på den relevante dag
+    btnOption.style.backgroundColor = "#FB8B24";
+    btnOption.style.color = "white";
+    } 
   //Tilføjer en eventlistener som lytter efter klik
   btnOption.addEventListener("click", function() {
 
@@ -19,8 +25,8 @@ btnOptions.forEach(function(btnOption) {
   });
 });
 
-//Tjekker om den nuværende URL har det relevante ord som en del af den
-if (window.location.href.includes("program") || window.location.href.includes('scene') || window.location.href.includes('schedule'))
+//Tjekker om den nuværende URL har det relevante ord som en del af den.
+if (window.location.href.includes("program") || window.location.href.includes('scene') || window.location.href.includes('Scene'))
 
 {
     //vælger ikoner i navigation og styler den som aktiv
@@ -31,31 +37,5 @@ if (window.location.href.includes("program") || window.location.href.includes('s
     let programPara = document.querySelector ("#programIkon p")
     programPara.style.color = "#FB8B24";
   };
-
-
-
-  
-// //Finder de relevante button og list og gemmer dem i variabler
-// let allButtons = document.querySelectorAll(".option"); 
-// let allLists = document.querySelectorAll(".programList"); 
-
-// // Lytter efter click på alle knapperne via forEach, og når en knap bliver trykket på, looper den igennem programlists og gemmer i et array
-// allButtons.forEach(function(button) {
-//     button.addEventListener("click", function() {
-  
-//       allLists.forEach(function(list) {
-//         let itemsArray = Array.from(list.children);
-  
-//         // Bruger math.random til at sortere listerne tilfæligt
-//         itemsArray.sort(function() {
-//           return Math.random() - 0.5;
-//         });
-  
-//         // Tømmer indholdet i listerne og sætte nye placeringer ind
-//         list.innerHTML = "";
-//         list.append(...itemsArray);
-//       });
-//     });
-//   });
  
 

8abe4de1d6c5f91ceddc13e294776547a2f46534
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-02-25 16:04:39 +0100

    finished JS. Began venue info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
+//Vælger de relevante knapper i HTML
+let btnOptions = document.querySelectorAll('.option');
 
+//Looper igennem hver knap med forEach
+btnOptions.forEach(function(btnOption) {
+
+  //Tilføjer en eventlistener som lytter efter klik
+  btnOption.addEventListener("click", function() {
+
+    //Sikre at kun den klikkede knap har er aktiv og er farvet anderledes end de andre. Nulstiller alle knapper 
+    btnOptions.forEach(function(otherBtn) {
+        otherBtn.style.backgroundColor = "";
+        otherBtn.style.color = "";
+    });
+
+    // Farverne som skal tilføjes til den valgte knap
+    this.style.backgroundColor = "#FB8B24";
+    this.style.color = "white";
+  });
+});
+
+//Tjekker om den nuværende URL har det relevante ord som en del af den
+if (window.location.href.includes("program") || window.location.href.includes('scene') || window.location.href.includes('schedule'))
+
+{
+    //vælger ikoner i navigation og styler den som aktiv
+    let program = document.querySelector("#programIkon i");
+    program.style.color = "#FB8B24";
+
+    //vælger p elementet i navigationen og styler den som aktiv
+    let programPara = document.querySelector ("#programIkon p")
+    programPara.style.color = "#FB8B24";
+  };
+
+
+
+  
 // //Finder de relevante button og list og gemmer dem i variabler
 // let allButtons = document.querySelectorAll(".option"); 
 // let allLists = document.querySelectorAll(".programList"); 
@@ -23,35 +59,3 @@
 //   });
  
 
-
-//Vælger de relevante knapper i HTML
-let btnOptions = document.querySelectorAll('.option');
-
-//Looper igennem hver knap med forEach
-btnOptions.forEach(function(btnOption) {
-
-  //Tilføjer en eventlistener som lytter efter klik
-  btnOption.addEventListener("click", function() {
-
-    //Sikre at kun den klikkede knap har er aktiv og er farvet anderledes end de andre. Nulstiller alle knapper 
-    btnOptions.forEach(function(otherBtn) {
-        otherBtn.style.backgroundColor = "";
-        otherBtn.style.color = "";
-    });
-
-    // Farverne som skal tilføjes til den valgte knap
-    this.style.backgroundColor = "#FB8B24";
-    this.style.color = "white";
-  });
-});
-
-//Tjekker om den nuværende URL har "program" som en del af den
-if (window.location.href.includes('program')) {
-    //vælger ikoner i navigation og styler den som aktiv
-    let program = document.querySelector("#programIkon i");
-    program.style.color = "#FB8B24";
-
-    //vælger p elementet i navigationen og styler den som aktiv
-    let programPara = document.querySelector ("#programIkon p")
-    programPara.style.color = "#FB8B24";
-  }

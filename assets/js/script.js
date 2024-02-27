262486d9048c5c0739908b5c3fa6c47450039201
Trine Kristensen <42169197+trin4988@users.noreply.github.com>
2024-02-27 21:24:18 +0100

    better

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJpbjQ5ODgiLCJhIjoiY2x0NGJzbnNwMDFjMTJsbW45ZmNsZ214aSJ9.U07zZkXyWrTV9r43obqs1w';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/trin4988/clt4bumwk00fp01pj0kt1h600',
+        center: [9.914406, 57.047314, 22], // starting position [lng, lat]
+        zoom: 12// starting zoom
+        
+    }); 
+
